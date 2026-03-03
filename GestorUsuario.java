4e214db302db788c8721c3c204a8af101e87f924
Dana <arrubladana@gmail.com>
2026-03-02 21:44:35 -0500

    Se añade la clse ususario y gestor de usuario

---
+++ GestorUsuario.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class GestorUsuario {
+
+    private HashMap<Integer, Usuario> usuariosMap;
+
+    public GestorUsuario() {
+        usuariosMap = new HashMap<>();
+    }
+
+    // Método para registrar usuario
+    public String registrarUsuario(Usuario usuario) {
+    // Que se ingresr todos los campos
+        if (usuario.getDocumento() == 0 ||
+            usuario.getNombreCompleto() == null || usuario.getNombreCompleto().isEmpty() ||
+            usuario.getTipoUsuario() == null || usuario.getTipoUsuario().isEmpty()) {
+
+            return "Todos los campos son obligatorios";
+        } // Verifica que no haya un documento repetido
+        else if (usuariosMap.containsKey(usuario.getDocumento())) {
+            return "Ya existe un usuario con ese documento";
+        }// Valida el tipo de usuario
+        else if (!usuario.getTipoUsuario().equalsIgnoreCase("Administrador") &&!usuario.getTipoUsuario().equalsIgnoreCase("Bibliotecario") &&!usuario.getTipoUsuario().equalsIgnoreCase("Lector")) {
+            return "Tipo de usuario no válido";
+
+        } else {
+                //ingresa el usuacrio
+            usuariosMap.put(usuario.getDocumento(), usuario);
+            return "Usuario registrado correctamente.";
+        }
+    }
+}
