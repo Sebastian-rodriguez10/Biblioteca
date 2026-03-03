d7bafd7fcc5722b3393ab688d0035b91041a2a8a
sebastian rodriguez <juanserodriguez711@gmail.com>
2026-03-02 23:39:23 -0500

    Realize la clase de lirbo y clase gestion de libros con sus validaciones y metodos

---
+++ GestionLibros.java
@@ -0,0 +1,90 @@
+import java.time.Year;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GestionLibros {
+
+    private Map<String, Libro> libros = new HashMap<>();
+
+    public void registrarLibro(String isbn, String titulo, String autor, int año, String estado) {
+
+        int añoActual = Year.now().getValue();
+
+        if (isbn.isEmpty() || titulo.isEmpty() || autor.isEmpty() || estado.isEmpty()) {
+            System.out.println("Ningun campo puede estar vacio");
+            return;
+        }
+
+        if (año > añoActual) {
+            System.out.println("El año no puede ser mayor al actual");
+            return;
+        }
+
+        if (libros.containsKey(isbn)) {
+            System.out.println("El ISBN ya está registrado");
+            return;
+        }
+
+        if (!estado.equalsIgnoreCase("Disponible") &&
+            !estado.equalsIgnoreCase("Prestado")) {
+            System.out.println("Estado invalido");
+            return;
+        }
+
+        Libro libro = new Libro(isbn, titulo, autor, año, estado);
+        libros.put(isbn, libro);
+
+        System.out.println("Libro registrado correctamente");
+    }
+
+    public void listarLibros() {
+
+        if (libros.isEmpty()) {
+            System.out.println("No hay libros registrados.");
+            return;
+        }
+
+        for (Libro libro : libros.values()) {
+            System.out.println(libro);
+        }
+    }
+
+    public void buscarLibro(String isbn) {
+
+        Libro libro = libros.get(isbn);
+
+        if (libro != null) {
+            System.out.println(libro);
+        } else {
+            System.out.println("Libro no encontrado.");
+        }
+    }
+
+    public void actualizarEstado(String isbn, String nuevoEstado) {
+
+        Libro libro = libros.get(isbn);
+
+        if (libro == null) {
+            System.out.println("Libro no encontrado");
+            return;
+        }
+
+        if (!nuevoEstado.equalsIgnoreCase("Disponible") &&
+            !nuevoEstado.equalsIgnoreCase("Prestado")) {
+            System.out.println("Estado inválido");
+            return;
+        }
+
+        libro.setEstado(nuevoEstado);
+        System.out.println("Estado actualizado correctamente.");
+    }
+
+    public void eliminarLibro(String isbn) {
+
+        if (libros.remove(isbn) != null) {
+            System.out.println("Libro eliminado correctamente");
+        } else {
+            System.out.println("Libro no encontrado");
+        }
+    }
+}
---
+++ Libro.java
@@ -0,0 +1,45 @@
+public class Libro {
+
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int añoPublicacion;
+    private String estado;
+
+    public Libro(String isbn, String titulo, String autor, int añoPublicacion, String estado) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.añoPublicacion = añoPublicacion;
+        this.estado = estado;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnioPublicacion() {
+        return añoPublicacion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "\nISBN: " + isbn + "\nTítulo: " + titulo + "\nAutor: " + autor + "\nAño de Publicación: " + añoPublicacion + "\nEstado: " + estado;
+    }
+}
